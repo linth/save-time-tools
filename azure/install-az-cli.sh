0f3e799f6f22820edd81ac5308581ed85cc5a55a
linth <zhua0404@gmail.com>
2022-01-03 16:44:39 +0800

    add a shell script for install az cli.

---
+++ azure/install-az-cli.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+# Reference:
+# https://docs.microsoft.com/zh-tw/cli/azure/install-azure-cli-linux?pivots=apt#option-1-install-with-one-command
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
