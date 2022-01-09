8ec578eedaad9771810693fdc605748bf8b4e438
linth <zhua0404@gmail.com>
2022-01-09 22:25:30 +0800

    add case example for shell script.

---
+++ shell-example/case.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+# Reference: http://linux.vbird.org/linux_basic/0340bashshell-scripts.php
+
+case $1 in 
+    "one")
+        echo "one";;
+    "two")
+        echo "two";;
+    "three")
+        echo "three";;
+esac
+
