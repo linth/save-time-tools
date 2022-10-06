903a0427efc7ddf124574a46dd13cff05703a274
George Lin <georgelin@oringnet.com>
2022-10-06 18:05:28 +0800

    install crud, pg, typeorm, @nestjs/crud.

---
+++ nestjs/templates/install-package/crud-pg-typeorm.sh
@@ -0,0 +1,10 @@
+
+# install nestjs/crud, validator
+npm i @nestjsx/crud class-transformer class-validator
+
+# install nestjs/typeorm, typeorm
+npm i @nestjsx/crud-typeorm @nestjs/typeorm typeorm
+
+# install postgres
+npm i pg
+
