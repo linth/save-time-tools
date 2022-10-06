70d02bc56d36d09f389580b05ac2e24a7b294a35
George Lin <georgelin@oringnet.com>
2022-10-06 18:04:48 +0800

    create a new nestjs project.

---
+++ nestjs/templates/sample-nestjs-project.sh
@@ -0,0 +1,23 @@
+# install the package of nest cli.
+echo "install the package of nest cli by npm."
+# npm i -g @nestjs/cli
+
+# build a new nestjs project
+echo "build a new nestjs project"
+nest new firstTestNestjs
+
+# yes for npm, TODO: yes for pass the button of enter.
+# expect "npm" { send "\r" }
+
+# into the folder
+# cd firstTestNestjs
+
+# install nestjs/crud, validator
+# npm i @nestjsx/crud class-transformer class-validator
+
+# install nestjs/typeorm, typeorm
+# npm i @nestjsx/crud-typeorm @nestjs/typeorm typeorm
+
+# install postgres
+# npm i pg
+
