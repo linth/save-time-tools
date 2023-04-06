623fb7f7fffb243ebb2d1f8b4878f1ba61e8cf1c
gerogelin <gerogelin@oringnet.com>
2023-04-06 17:50:19 +0800

    update the process example.

--- utils/db/check_process_status.py
+++ utils/db/check_process_status.py
@@ -1,9 +1,19 @@
+'''
+check the process is running or not.
+'''
 import psutil
 
-def check_forticlient_running():
+
+def is_running_for_process(*args, **kwargs):
+    # print(args)
+    # print(type(args[0]))
+
+    if (type(args[0]) != str):
+        return False    
+
     for proc in psutil.process_iter():
         try:
-            if proc.name() == "FortiClient.exe":
+            if proc.name() == args[0]:
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
@@ -11,8 +21,12 @@ def check_forticlient_running():
 
 
 if __name__ == '__main__':
-    if check_forticlient_running():
+    # if check_forticlient_running():
+    #     print("FortiClient is running.")
+    # else:
+    #     print("FortiClient is not running.")
+
+    if is_running_for_process('FortiClient.exe'):
         print("FortiClient is running.")
     else:
         print("FortiClient is not running.")
-
